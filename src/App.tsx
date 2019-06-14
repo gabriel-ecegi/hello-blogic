3d6ae3bf35b5439eccf66fb6bbff8109ffc3b533
Gabriel Ecegi <gabriel.ecegi@blogic.cz>
2019-06-14 13:06:33 +0200

    Use hooks again

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
-type State = {
-	name: string;
+const App: React.FC = () => {
+	const [name, setName] = useState("");
+	return (
+		<div className="App">
+			<input value={name} onChange={e => setName(e.target.value)} />
+			<p>Hodnota v inpute je {name}</p>
+		</div>
+	);
 };
-
-class App extends React.Component<{}, State> {
-	state = { name: "" };
-
-	setName = (name: string) => {
-		this.setState({ name });
-	};
-
-	render() {
-		return (
-			<div className="App">
-				<input
-					value={this.state.name}
-					onChange={e => this.setName(e.target.value)}
-				/>
-				<p>Hodnota v inpute je {this.state.name}</p>
-			</div>
-		);
-	}
-}
 
 export default App;
