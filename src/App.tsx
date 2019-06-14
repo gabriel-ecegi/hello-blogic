48dd4240de060f77183b9a588fb711b4590aff10
Gabriel Ecegi <gabriel.ecegi@blogic.cz>
2019-06-14 12:56:47 +0200

    Add hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+	const [name, setName] = useState("");
+	return (
+		<div className="App">
+			<input value={name} onChange={e => setName(e.target.value)} />
+			<p>Hodnota v inpute je {name}</p>
+		</div>
+	);
+};
 
 export default App;
